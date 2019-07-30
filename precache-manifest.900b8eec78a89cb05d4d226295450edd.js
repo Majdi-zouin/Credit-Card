2c6f3b42235ffc1f0d7339dcf9f84e8737a026de
Majdi-zouin <majdizouin@gmail.com>
2019-07-30 16:11:06 +0200

    Updates

---
+++ precache-manifest.900b8eec78a89cb05d4d226295450edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e875e24ac2baeee2a964e588375c3545",
+    "url": "/Credit-Card/index.html"
+  },
+  {
+    "revision": "d21d3db7b5cdaea2cdbb",
+    "url": "/Credit-Card/static/css/main.e1897902.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Credit-Card/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d21d3db7b5cdaea2cdbb",
+    "url": "/Credit-Card/static/js/main.7caff821.chunk.js"
+  },
+  {
+    "revision": "1425aa8bd00b6e03511e",
+    "url": "/Credit-Card/static/js/runtime~main.35f599d6.js"
+  },
+  {
+    "revision": "c1be93bb15c175b027ec262c93e0cbd7",
+    "url": "/Credit-Card/static/media/bck.c1be93bb.jpg"
+  }
+]);
